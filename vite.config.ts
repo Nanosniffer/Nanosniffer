ca9eefbd8cb7bd600fc7bfde90298b614f89a316
Taxil Pambhar <taxilpambhar@gmail.com>
2026-08-26 19:25:21 +0530

    feat(repo): update base path and assets for nanosniffer repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/criminal-network-analysis-system/',
+  base: '/nanosniffer/',
   plugins: [react()],
   resolve: {
     alias: {
